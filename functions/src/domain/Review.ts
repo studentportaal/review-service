019374b0b2d5fc396c3d159284480fb88b5fb1c5
MeijerM1 <max1_meijer@hotmail.com>
2019-05-23 17:50:27 +0200

    Add review getting

--- functions/src/domain/Review.ts
+++ functions/src/domain/Review.ts
@@ -1,3 +1,9 @@
 export class Review {
-
+    // Author of the review, Either a student or Company uuid
+    author: string = '';
+    // Target of the review, Either a student or Company uuid
+    target: string = '';
+    content: string = '';
+    postDate: Date = new Date();
+    stars: number = 0;
 }
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,46 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+import {Review} from "./domain/Review";
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
 export const addReview = functions.https.onRequest((request, response) => {
     admin.initializeApp();
-    response.send("Hello from Firebase!");
+    const review: Review = request.body;
+
+    if (review.postDate == undefined) {
+        review.postDate = new Date();
+    }
+
+    admin.firestore().collection('reviews').add(review).then(writeResult => {
+        response.status(201).send(writeResult);
+    }).catch(err => {
+        response.status(400).send(err);
+    });
 });
+
+export const getReviews = functions.https.onRequest((request, response) => {
+    admin.initializeApp();
+    const target = request.query.target;
+
+    if (!target) {
+        response.status(400).send('No target specified in query params')
+    }
+
+    admin.firestore().collection('reviews')
+        .where('target', '==', target)
+        .get().then(snapshot => {
+        if (!snapshot.empty) {
+            let reviews: any = [];
+            snapshot.docs.forEach(el => {
+                reviews.push(el.data())
+            });
+            response.status(200).send(reviews);
+        }
+
+    }).catch(err => {
+        response.status(400).send(err);
+    })
+});
+
