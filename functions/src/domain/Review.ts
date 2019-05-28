b59364526e56eb7a73b8996ef45dbf171540053f
MeijerM1 <max1_meijer@hotmail.com>
2019-05-28 11:08:15 +0200

    Add review validation

--- functions/src/domain/Review.ts
+++ functions/src/domain/Review.ts
@@ -1,9 +1,22 @@
+import {IsInt, IsNotEmpty, Max, Min, IsDate} from "class-validator";
+
 export class Review {
     // Author of the review, Either a student or Company uuid
+    @IsNotEmpty()
     author: string = '';
     // Target of the review, Either a student or Company uuid
+    @IsNotEmpty()
     target: string = '';
     content: string = '';
+
+    @IsDate()
     postDate: Date = new Date();
+
+    @IsInt()
+    @Min(1)
+    @Max(5)
     stars: number = 0;
+
+    @IsNotEmpty()
+    isWritten: boolean = false;
 }
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,38 +1,49 @@
 import * as functions from 'firebase-functions';
-import * as admin from 'firebase-admin';
 import {Review} from "./domain/Review";
+import {validate} from "class-validator";
+import firebase from './Firebase'
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
 export const addReview = functions.https.onRequest((request, response) => {
-    admin.initializeApp();
-    const review: Review = request.body;
+    const review: Review = new Review();
+    review.postDate = new Date();
+    review.author = request.body.author;
+    review.target = request.body.target;
+    review.content = request.body.content;
+    review.isWritten = request.body.isWritten;
+    review.stars = request.body.stars;
 
-    if (review.postDate == undefined) {
-        review.postDate = new Date();
-    }
+    validate(review).then(errors => {
+        if (errors.length > 0) {
+            response.status(400).send('Invalid json object')
+        }
+    }).catch(error => {
+        response.status(500).send('Error parsing json')
+    });
 
-    admin.firestore().collection('reviews').add(review).then(writeResult => {
-        response.status(201).send(writeResult);
+    const data = JSON.parse(JSON.stringify(review));
+
+    firebase.firestore().collection('reviews').add(data).then(writeResult => {
+        response.status(201).send();
     }).catch(err => {
         response.status(400).send(err);
     });
 });
 
 export const getReviews = functions.https.onRequest((request, response) => {
-    admin.initializeApp();
     const target = request.query.target;
 
     if (!target) {
         response.status(400).send('No target specified in query params')
     }
 
-    admin.firestore().collection('reviews')
+    firebase.firestore().collection('reviews')
         .where('target', '==', target)
         .get().then(snapshot => {
         if (!snapshot.empty) {
-            let reviews: any = [];
+            const reviews: any = [];
             snapshot.docs.forEach(el => {
                 reviews.push(el.data())
             });
